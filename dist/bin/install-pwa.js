19171afecc34e463975e8482fed06995f6e6e81c
Prabhas Kurapati <prabhask@berkeley.edu>
2026-03-02 01:55:34 +0000

    v1.1.17

--- dist/bin/install-pwa.js
+++ dist/bin/install-pwa.js
@@ -206,7 +206,7 @@ function generatePackageJson(opts) {
         },
         dependencies: {
             postgres: '^3.4.0',
-            'stellar-drive': '^1.1.16'
+            'stellar-drive': '^1.1.17'
         },
         type: 'module'
     }, null, 2) + '\n');
